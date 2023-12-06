889523b0e7fb941b6ac7143eca992b5b6ca7a19b
A252431 <A252431@LSBSA-C26NHW3>
2023-12-06 13:44:25 +0200

    Add project files.

---
+++ 2.Draw a shape/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2.Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /   |");
+            Console.WriteLine("  -----");
+
+        }
+    }
+}
